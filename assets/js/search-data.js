d5e5e7eead89fbf3def61024c289e668ae785ab9
gorjanradevski <36923013+gorjanradevski@users.noreply.github.com>
2025-03-26 12:10:36 +0000

    Deploying to gh-pages from @ gorjanradevski/gorjanradevski.github.io@2f8d40712d0a429102ef64443b89d74c804aaee0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,18 +458,18 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/gorjan-radevski", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-stackoverflow',
+        title: 'Stackoverflow',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://stackoverflow.com/users/3987085", "_blank");
         },
       },{
       id: 'light-theme',

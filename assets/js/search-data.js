a5be209fc13c474ee8402791b43d1b45fcc522c8
gorjanradevski <36923013+gorjanradevski@users.noreply.github.com>
2025-09-18 17:53:57 +0000

    Deploying to gh-pages from @ gorjanradevski/gorjanradevski.github.io@c162843b83c389cc02f24d21004c8783f6e3c860 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-curriculum-vitae",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-curriculum-vitae",
           title: "curriculum vitae",
           description: "",
           section: "Navigation",
